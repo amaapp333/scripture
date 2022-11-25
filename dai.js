f6036694d7c94d6abba133b2732705c1fdb8c12b
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-25 18:50:02 +0200

    Update dai.js

--- dai.js
+++ dai.js
@@ -1,58 +1,19 @@
-var streamManager;   // used to request ad-enabled streams.
-  var hls = new Hls(); // hls.js video player
-  var videoElement;
-  var adUiElement;
-  var timerElement
+var BACKUP_STREAM =
+    'https://storage.googleapis.com/interactive-media-ads/media/bbb.m3u8'
 
-  ...
+// Live stream asset key.
+var TEST_ASSET_KEY = "sN_IYUG8STe1ZzhIIE_ksA";
 
-  function initPlayer() {
-    videoElement = document.getElementById('video');
-    adUiElement = document.getElementById('adUi');
-    timerElement = document.getElementById('ad-timer');
+// VOD content source and video IDs.
+var TEST_CONTENT_SOURCE_ID = "2528370";
+var TEST_VIDEO_ID = "tears-of-steel";
 
-  ...
+var hls = new Hls(); // hls.js video player
+var videoElement;
+var adUiElement;
+var isAdBreak;
 
-  streamManager.addEventListener(
-    [google.ima.dai.api.StreamEvent.Type.LOADED,
-     google.ima.dai.api.StreamEvent.Type.ERROR,
-     google.ima.dai.api.StreamEvent.Type.AD_BREAK_STARTED,
-     google.ima.dai.api.StreamEvent.Type.AD_BREAK_ENDED,
-     google.ima.dai.api.StreamEvent.Type.PAUSED,
-     google.ima.dai.api.StreamEvent.Type.RESUMED,
-     google.ima.dai.api.StreamEvent.Type.AD_PROGRESS],
-    onStreamEvent,
-    false);
-
-  ...
-
-  function onStreamEvent(e) {
-    switch (e.type) {
-      case google.ima.dai.api.StreamEvent.Type.LOADED:
-        console.log('Stream loaded');
-        loadUrl(e.getStreamData().url);
-        break;
-      case google.ima.dai.api.StreamEvent.Type.ERROR:
-        console.log('Error loading stream, playing backup stream.' + e);
-        loadUrl(BACKUP_STREAM);
-        break;
-      case google.ima.dai.api.StreamEvent.Type.AD_BREAK_STARTED:
-        console.log('Ad Break Started');
-        videoElement.controls = false;
-        adUiElement.style.display = 'block';
-        break;
-      case google.ima.dai.api.StreamEvent.Type.AD_PROGRESS:
-        var progressData = e.getStreamData().adProgressData;
-        var timeRemaining = Math.ceil(progressData.duration - progressData.currentTime);
-        timerElement.innerHTML = 'Ad finished in: ' + timeRemaining;
-        break;
-      case google.ima.dai.api.StreamEvent.Type.AD_BREAK_ENDED:
-        timerElement.innerHTML = 'Ad not currently playing.';
-        console.log('Ad Break Ended');
-        videoElement.controls = true;
-        adUiElement.style.display = 'none';
-        break;
-      default:
-        break;
-    }
-  }
+function initPlayer() {
+  videoElement = document.getElementById('video');
+  adUiElement = document.getElementById('adUi');
+}
